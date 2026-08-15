38664059fb29486b326b698396661939f380eff3
Intellect.lol <zecke646@gmail.com>
2026-08-14 17:59:18 -0700

    Update supportedGames.lua

--- supportedGames.lua
+++ supportedGames.lua
@@ -1,7 +1,7 @@
 local supportedGames = {}
 
 supportedGames.BLOXSTRIKE = {
-	placeIDs = { 114234929420007 },
+	placeIDs = { 114234929420007, 135434213652028, 108194354348181 },
 	gitPath = "Games/Bloxstrike",
 
 	gameName = "BloxStrike",
