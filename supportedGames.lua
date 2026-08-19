ddb7fc67c1c7f85f15df2c7081d113ff61eedb3b
Bismuth <zecke646@gmail.com>
2026-08-19 16:22:25 -0700

    Remove BloxStrike from supported games
    Removed BloxStrike game details from supported games list.

--- supportedGames.lua
+++ supportedGames.lua
@@ -1,26 +1,5 @@
 local supportedGames = {}
 
-supportedGames.BLOXSTRIKE = {
-	placeIDs = { 114234929420007, 135434213652028, 108194354348181 },
-	gitPath = "Games/Bloxstrike",
-
-	gameName = "BloxStrike",
-	status = "Undetected",
-
-	defaultExecutorStatus = "Undetected",
-	executorStatus = {
-		Potassium = "Undetected",
-		Volt = "Undetected",
-		Synz = "Undetected",
-
-		Madium = "Detected",
-		Velocity = "Detected",
-		Xeno = "Detected",
-		Solara = "Detected",
-		Real = "Detected",
-	},
-}
-
 supportedGames.OPERATIONONE = {
 	placeIDs = { 72920620366355 },
 	gitPath = "Games/OperationOne",
