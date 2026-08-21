4bc2f9e4abee485cd035d72d1cb086eb586b9c89
Bismuth <zecke646@gmail.com>
2026-08-21 05:32:43 -0700

    Add BloxStrike and Vagrant Survival to supported games
    Added support for BloxStrike and Vagrant Survival games with their respective place IDs, git paths, and executor statuses.

--- supportedGames.lua
+++ supportedGames.lua
@@ -1,10 +1,52 @@
 local supportedGames = {}
 
+supportedGames.BLOXSTRIKE = {
+	placeIDs = { 114234929420007, 135434213652028, 108194354348181 },
+	gitPath = "Games/Bloxstrike",
+
+	gameName = "BloxStrike",
+	status = "Undetected",
+
+	defaultExecutorStatus = "Down",
+	executorStatus = {
+		Potassium = "Down",
+		Volt = "Down",
+		Synz = "Down",
+		Madium = "Down",
+		Velocity = "Down",
+		Xeno = "Down",
+		Solara = "Down",
+		Real = "Down",
+	},
+}
+
 supportedGames.OPERATIONONE = {
 	placeIDs = { 72920620366355 },
 	gitPath = "Games/Operation%20One",
 
 	gameName = "Operation One",
+	status = "Undetected",
+
+	defaultExecutorStatus = "Undetected",
+	executorStatus = {
+		Potassium = "Undetected",
+		Volt = "Undetected",
+		Synz = "Undetected",
+		Real = "Undetected",
+
+		Madium = "Detected",
+		Velocity = "Detected",
+		Xeno = "Detected",
+		Solara = "Detected",
+	},
+}
+
+
+supportedGames.VAGRANT = {
+	placeIDs = { 100283815455755 },
+	gitPath = "Games/Vagrant%20Survival",
+
+	gameName = "Vagrant Survival",
 	status = "Undetected",
 
 	defaultExecutorStatus = "Undetected",
